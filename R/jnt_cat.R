1f215ff8159585f05adad61d21b721db5126a4ad
kenstoyama <ken.toyama@mail.utoronto.ca>
2023-01-17 19:09:05 +0100

    Update jnt_cat.R

--- R/jnt_cat.R
+++ R/jnt_cat.R
@@ -185,8 +185,8 @@ jnt_cat <- function(X,Y,g,data,plot.full=F,phylo=F,tree,cols=c("black","black"),
                                              max(data[,Y])*2,min(data[,Y])*0.5),col=rgb(224, 224, 224,
                                                                                         maxColorValue=255,alpha=130), border=NA)
   }
-  legend(par('usr')[1],par('usr')[4]+((par('usr')[4]-par('usr')[3])/5), bty='n', xpd=NA,
-         c("max mod value in data", "min mod value in data"),
+  legend(par('usr')[1],par('usr')[4]+((par('usr')[4]-par('usr')[3])/2.5), bty='n', xpd=NA,
+         c(levs[1], levs[2]),
          pch=c(sym[1],sym[2]),cex=0.5,col=c(cols[1],cols[2]))
   if(((B^2)-A*C)<0){
     results <- list("coeff" = mod.out)

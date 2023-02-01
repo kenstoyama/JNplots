4258d3372e60b16774e713f3d23e559b50b02ce0
kenstoyama <ken.toyama@mail.utoronto.ca>
2023-02-01 01:18:56 +0100

    Update jnt_cat.R

--- R/jnt_cat.R
+++ R/jnt_cat.R
@@ -177,9 +177,9 @@ jnt_cat <- function(X,Y,g,data,plot.full=F,phylo=F,tree,cols=c("black","black"),
                                              max(data[,Y])*2,-2*(abs(min(data[,Y])))),col=rgb(224, 224, 224,
                                                                                         maxColorValue=255,alpha=130), border=NA)
   }else{
-    plot(data[,X],data[,Y],xlab=X,ylab=Y,type="n",cex=cex)
+    plot(data[,X],data[,Y],xlab=X,ylab=Y,type="n")
     points(group1[,X],group1[,Y],col=cols[1],pch=sym[1],cex=cex)
-    points(group2[,X],group2[,Y],col=cols[2],pch=sym[2])
+    points(group2[,X],group2[,Y],col=cols[2],pch=sym[2],cex=cex)
     abline(a1,b1,lty=2)
     abline(a2,b2)
     polygon(c(xlower,xlower,xupper,xupper),c(-2*(abs(min(data[,Y]))),max(data[,Y])*2,

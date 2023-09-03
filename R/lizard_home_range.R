8e01de51dcad651d33e599ae5fd7e56f4db9c144
kenstoyama <ken.toyama@mail.utoronto.ca>
2023-09-04 00:51:26 +0200

    Update lizard_home_range.R

--- R/lizard_home_range.R
+++ R/lizard_home_range.R
@@ -12,6 +12,6 @@
 #' @keywords datasets
 #'
 #' @references Payne et al. (2021) Ecological Monographs https://doi.org/10.1002/ecm.1519
-#' (\href{https://pubag.nal.usda.gov/catalog/7860136}{PubAg})
+#' (\href{https://https://esajournals.onlinelibrary.wiley.com/doi/full/10.1002/ecm.1519)
 #'
 "lizard_home_range"
